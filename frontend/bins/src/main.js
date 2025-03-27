344d3145cbdd02b2b59cba83fdf00da86ff3c6aa
muriplz <46825658+muriplz@users.noreply.github.com>
2025-03-27 05:43:07 +0100

    Fix vue-i18n dependency warn

--- frontend/bins/src/main.js
+++ frontend/bins/src/main.js
@@ -2,13 +2,12 @@ import {createApp} from 'vue';
 import './style.css';
 import App from './App.vue';
 import router from './router';
-import AuthService from './js/auth/authService.js';
+import AuthService from './js/auth/AuthService.js';
 
 import {CarbonIconsVue, LogoDiscord32, LogoGithub32, Settings32, User32, UserProfile32,} from '@carbon/icons-vue';
 
 import settingsStore from '@/components/ui/settings/settings.js';
 import {initGPSSystem} from "@/components/player/GPSTracker.js";
-import ItemsApi from "@/js/items/ItemsApi.js";
 import Store from "@/js/Store.js";
 import {createI18n} from "vue-i18n";
 
@@ -19,6 +18,7 @@ const browserLanguage = navigator.language.split('-')[0];
 const savedLanguage = localStorage.getItem('language') || browserLanguage || 'en';
 
 export const i18n = createI18n({
+    legacy: false,
     locale: savedLanguage,
     fallbackLocale: 'en',
     globalInjection: true,
