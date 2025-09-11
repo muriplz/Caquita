e9e7a8d4d40440d30420bd7d2ba35b556e989b0c
muriplz <46825658+muriplz@users.noreply.github.com>
2025-09-11 14:09:40 +0200

    Fix orbitting on menu

--- frontend/ashtray/scenes/game/ui/menu_button.gd
+++ frontend/ashtray/scenes/game/ui/menu_button.gd
@@ -1,11 +1,12 @@
 extends Button
-
 @onready var menu = $"../../Menu"
 @onready var ui: Control = $"../.."
 
 func _ready():
 	pressed.connect(self._on_button_pressed)
 
-
 func _on_button_pressed() -> void:
 	ui.toggle_screen(menu)
+	var camera = get_node("/root/Game/Player/Camera3D")
+	if camera:
+		camera.input_disabled = menu.visible
